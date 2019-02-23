7e51cb571c96b71b66712913ae90cd42a329e892
Andreas Tully <Andreas.Tully@deutschebahn.com>
2019-02-23 23:46:35 +0100

    fix color decoding

--- fb2canvas.py
+++ fb2canvas.py
@@ -30,35 +30,35 @@ while 1:
       x+=1
       if (x % WIDTH) == 0:
          x=0;y+=1
-      if 16 > b[i] > 0:
+      if 16 > b[i] >= 0:
          canvas.create_rectangle( (x, y)*2 , outline='#003333')
-      elif 32 > b[i] > 16:
+      elif 32 > b[i] >= 16:
          canvas.create_rectangle( (x, y)*2 , outline='#004444')
-      elif 48 > b[i] > 32:
+      elif 48 > b[i] >= 32:
          canvas.create_rectangle( (x, y)*2 , outline='#005555')
-      elif 64 > b[i] > 48:
+      elif 64 > b[i] >= 46:
          canvas.create_rectangle( (x, y)*2 , outline='#006666')
-      elif 80 > b[i] > 64:
+      elif 80 > b[i] >= 63:
          canvas.create_rectangle( (x, y)*2 , outline='#007777')
-      elif 96 > b[i] > 80:
+      elif 96 > b[i] >= 80:
          canvas.create_rectangle( (x, y)*2 , outline='#008888')
-      elif 112 > b[i] > 96:
+      elif 112 > b[i] >= 96:
          canvas.create_rectangle( (x, y)*2 , outline='#00aaaa')
-      elif 128 > b[i] > 112:
+      elif 128 > b[i] >= 112:
          canvas.create_rectangle( (x, y)*2 , outline='#66ffff')
-      elif 144 > b[i] > 128:
+      elif 144 > b[i] >= 128:
          canvas.create_rectangle( (x, y)*2 , outline='#77ffff')
-      elif 160 > b[i] > 144:
+      elif 160 > b[i] >= 144:
          canvas.create_rectangle( (x, y)*2 , outline='#88ffff')
-      elif 176 > b[i] > 160:
+      elif 176 > b[i] >= 160:
          canvas.create_rectangle( (x, y)*2 , outline='#99ffff')
-      elif 192 > b[i] > 176:
+      elif 192 > b[i] >= 176:
          canvas.create_rectangle( (x, y)*2 , outline='#aaffff')
-      elif 208 > b[i] > 192:
+      elif 208 > b[i] >= 192:
          canvas.create_rectangle( (x, y)*2 , outline='#bbffff')
-      elif 224 > b[i] > 208:
+      elif 224 > b[i] >= 208:
          canvas.create_rectangle( (x, y)*2 , outline='#ccffff')
-      elif 240 > b[i] > 224:
+      elif 240 > b[i] >= 224:
          canvas.create_rectangle( (x, y)*2 , outline='#ddffff')
       else:
          canvas.create_rectangle( (x, y)*2 , outline='#ffffff')
